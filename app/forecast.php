a027ba1ddcb53fb6b7bc7c830c7c31d8c58a8224
Pete Medina <petemedinacreative@gmail.com>
2017-09-25 17:53:18 -0700

    Added key.php variable

--- app/forecast.php
+++ app/forecast.php
@@ -1,7 +1,7 @@
 <?php
 
   // call the weather api, pass the lat_lng_array
-  $wather = 'https://api.darksky.net/forecast/YOUR-API-KEY/34.2381,-118.5301/?exclude=minutely?exclude=hourly';
+  $wather = 'https://api.darksky.net/forecast/'.$key.'/34.2381,-118.5301/?exclude=minutely?exclude=hourly';
 
   // get the data for the forcast
   $forecast = json_decode(file_get_contents($wather), true);
--- index.php
+++ index.php
@@ -4,5 +4,6 @@
    * @author Pete Medina
    */
 
+  require_once 'app/key.php';
   require_once 'app/forecast.php';
   require_once 'views/home.php';
